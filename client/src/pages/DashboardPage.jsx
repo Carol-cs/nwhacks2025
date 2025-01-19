161840fd505c61d0fab1344c256afa721b2fa35b
Carol Ma <carol.ma3728@gmail.com>
2025-01-18 18:43:23 -0800

    setup api call

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useGetUsersQuery } from "../slices/usersApiSlice";
 
 const DashboardPage = () => {
+  const { data: users, isLoading, error } = useGetUsersQuery();
   return (
     <div>
       <h1>DashboardPage</h1>
@@ -12,6 +14,23 @@ const DashboardPage = () => {
           </li>
         </ul>
       </nav>
+      <h1>All Users</h1>
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <div>{error?.data?.message || error.error}</div>
+      ) : (
+        <>
+          <ul>
+            {users.map((user) => (
+              <li key={user._id}>
+                <h2>{user.name}</h2>
+                <p>{user.email}</p>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
     </div>
   );
 };
---
+++ client/src/slices/apiSlice.js
@@ -0,0 +1,11 @@
+// parent to our other API slices
+
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { BASE_URL } from "../utils/constants";
+
+const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
+export const apiSlice = createApi({
+  baseQuery,
+  tagTypes: ["User", "ChatLog", "HealthInfo"],
+  endpoints: (builder) => ({}),
+});
